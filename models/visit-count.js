3e0b6615f3ce7726fb2cd17d6093699b5ddd4136
Andrey Allyson <andreyuser47@gmail.com>
2025-05-18 01:18:47 -0300

    fix: fix concurrent requests by syncronizing Count.next

--- models/visit-count.js
+++ models/visit-count.js
@@ -4,12 +4,35 @@ const path = require("node:path");
 const Count = {};
 module.exports = Count;
 
+const queue = [];
+
+async function lock() {
+  return new Promise((res) => {
+    const i = queue.push(res);
+
+    if (i === 1) {
+      res();
+    }
+  });
+}
+
+function unlock() {
+  queue.shift();
+  if (queue.length > 0) {
+    const next = queue[0];
+
+    next();
+  }
+}
+
 Count.next = async () => {
   const visitsPath = path.resolve(process.cwd(), ".data", "visits.txt");
 
+  await lock();
   await fs.mkdir(path.dirname(visitsPath), { recursive: true });
 
   const file = await fs.open(visitsPath, "a+");
+
   try {
     let buffer = Buffer.alloc(32);
 
@@ -36,6 +59,7 @@ Count.next = async () => {
 
     return json.count;
   } finally {
+    unlock();
     await file.close();
   }
 };
